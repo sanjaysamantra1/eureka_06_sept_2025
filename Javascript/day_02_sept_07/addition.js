2ce751ad46d19f319e8e7b46b4fe9c697aa5b5bf
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-09-07 20:44:57 +0530

    day 02

---
+++ Javascript/day_02_sept_07/addition.js
@@ -0,0 +1,5 @@
+let a = 10;
+let b = 20;
+
+console.log(`Addition of ${a} and ${b} is ${a + b}`);  // valid in both browser + nodejs
+window.alert(`Addition of ${a} and ${b} is ${a + b}`); // only valid in browser
